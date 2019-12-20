61fb924e1ac244cad6282be75544f1790f87d07b
hazemkhairy <hazemkhairy188@gmail.com>
2019-12-20 21:55:34 +0200

    added user and category services

---
+++ OSBE/API/CategoryService.svc.cs
@@ -0,0 +1,31 @@
+﻿using OSBE.BLL;
+using OSBE.DTOs;
+using OSBE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OSBE.API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CategoryService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CategoryService.svc or CategoryService.svc.cs at the Solution Explorer and start debugging.
+    public class CategoryService : ICategoryService
+    {
+        private CategoryBLL categoryBLL;
+        public  CategoryService()
+        {
+            categoryBLL = new CategoryBLL();
+        }
+        public List<CategoryDTO> GetAll()
+        {
+            return categoryBLL.GetAll();
+        }
+        public string Add(CategoryDTO category)
+        {
+            return categoryBLL.Add(category);
+        }
+    }
+}
---
+++ OSBE/BLL/UserBLL.cs
@@ -0,0 +1,64 @@
+﻿using OSBE.Context;
+using OSBE.DTOs;
+using OSBE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace OSBE.BLL
+{
+    public class UserBLL
+    {
+        OSContext _context;
+        public UserBLL()
+        {
+            _context = new OSContext();
+        }
+
+        public UserDTO Register(UserDTO user)
+        {
+            User user1 = _context.Users.FirstOrDefault(u => u.Email == user.Email);
+            if(user1!=null)
+            {
+                throw new WebFaultException<string>("Email Already Exists", HttpStatusCode.Conflict);
+            }
+            user1 = new User()
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Password = user.Password,
+                IsAdmin = false 
+            };
+
+            _context.Users.Add(user1);
+            _context.SaveChanges();
+            user.ID = user1.ID;
+            return user;
+        }
+        public int Login(UserDTO user)
+        {
+            User user1 = _context.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (user1 == null)
+            {
+                throw new WebFaultException<string>("Invalid Email or Password", HttpStatusCode.NotFound);
+            }
+
+            
+           return user1.ID;
+        }
+        public bool isAdmin (int id)
+        {
+            User user = _context.Users.FirstOrDefault(i => i.ID == id);
+            if (user != null && user.IsAdmin)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
